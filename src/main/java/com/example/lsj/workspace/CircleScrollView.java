d34a4ae02f30f1d77e9b444e470cf45d0f13c5f8
nikooli <shaojin8864@qq.com>
2015-12-20 22:38:48 +0800

    circlescrollview

--- src/main/java/com/example/lsj/workspace/CircleScrollView.java
+++ src/main/java/com/example/lsj/workspace/CircleScrollView.java
@@ -1,9 +1,11 @@
 package com.example.lsj.workspace;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
@@ -14,6 +16,17 @@ import android.view.ViewGroup;
  */
 public class CircleScrollView extends ViewGroup {
     private Paint mCirPaint;
+    private float mTextWidth = 0.0f;
+    private CircleView circleView;
+    /**
+     * RectF存储四个float类型的坐标值，分别为左、上、右、下
+     * 通过这四个值可以呈现一个矩形。
+     * 这里面的值可以直接存取，并且可以通过width()和height()函数
+     * 很方便的获得矩形的宽和高。注意，这里并没有判断值的合法性。
+     * 比如左比右大了。正确的为左<=右，上<=下
+     */
+    private RectF mCircleBounds = new RectF();
+
     public CircleScrollView(Context context) {
         super(context);
         init();
@@ -21,11 +34,31 @@ public class CircleScrollView extends ViewGroup {
 
     /**
      * 如果要在xml布局中使用该控件，必须重载带attr的构造函数
+     *
      * @param context
      * @param attrs
      */
     public CircleScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.CircleScrollView,
+                0, 0
+        );
+
+        try {
+            // Retrieve the values from the TypedArray and store into
+            // fields of this class.
+            //
+            // The R.styleable.PieChart_* constants represent the index for
+            // each custom attribute in the R.styleable.PieChart array.
+
+            mTextWidth = a.getDimension(R.styleable.CircleScrollView_labelWidth, 0.0f);
+
+        } finally {
+            // release the TypedArray so that it can be reused.
+            a.recycle();
+        }
         init();
     }
 
@@ -38,111 +71,58 @@ public class CircleScrollView extends ViewGroup {
         mCirPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mCirPaint.setColor(Color.parseColor("#232323"));
 
-        CircleView circleView = new CircleView(getContext());
+        circleView = new CircleView(getContext());
         addView(circleView);
+    }
+
+    @Override
+    protected int getSuggestedMinimumWidth() {
+        return (int) mTextWidth;
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int count = getChildCount();
-        // Try for a width based on our minimum
-        int measureWidth = measureWidth(widthMeasureSpec);
-        int measureHeight = measureHeight(heightMeasureSpec);
-        // 计算自定义的ViewGroup中所有子控件的大小
-        measureChildren(widthMeasureSpec, heightMeasureSpec);
-        // 设置自定义的控件MyViewGroup的大小
-        setMeasuredDimension(measureWidth, measureHeight);
-    }
-    private int measureWidth(int pWidthMeasureSpec) {
-        int result = 0;
-        int widthMode = MeasureSpec.getMode(pWidthMeasureSpec);// 得到模式
-        int widthSize = MeasureSpec.getSize(pWidthMeasureSpec);// 得到尺寸
+        //计算该ViewGroup的宽度
+        int minw = getPaddingLeft() + getPaddingRight() + getSuggestedMinimumWidth();
+        int w = Math.min(minw, MeasureSpec.getSize(widthMeasureSpec));
+        //计算该ViewGroup的高度
+        int minh = (w - (int) mTextWidth) + getPaddingBottom() + getPaddingTop();
+        int h = Math.max(MeasureSpec.getSize(heightMeasureSpec), minh);
 
-        switch (widthMode) {
-            /**
-             * mode共有三种情况，取值分别为MeasureSpec.UNSPECIFIED, MeasureSpec.EXACTLY,
-             * MeasureSpec.AT_MOST。
-             *
-             *
-             * MeasureSpec.EXACTLY是精确尺寸，
-             * 当我们将控件的layout_width或layout_height指定为具体数值时如andorid
-             * :layout_width="50dip"，或者为FILL_PARENT是，都是控件大小已经确定的情况，都是精确尺寸。
-             *
-             *
-             * MeasureSpec.AT_MOST是最大尺寸，
-             * 当控件的layout_width或layout_height指定为WRAP_CONTENT时
-             * ，控件大小一般随着控件的子空间或内容进行变化，此时控件尺寸只要不超过父控件允许的最大尺寸即可
-             * 。因此，此时的mode是AT_MOST，size给出了父控件允许的最大尺寸。
-             *
-             *
-             * MeasureSpec.UNSPECIFIED是未指定尺寸，这种情况不多，一般都是父控件是AdapterView，
-             * 通过measure方法传入的模式。
-             */
-            case MeasureSpec.AT_MOST:
-            case MeasureSpec.EXACTLY:
-                result = widthSize;
-                break;
-        }
-        return result;
+        setMeasuredDimension(w, h);
     }
 
-    private int measureHeight(int pHeightMeasureSpec) {
-        int result = 0;
+    /**
+     * 在这个函数中计算子View的尺寸大小，然后放置子View
+     * 也可以在onLayout函数中放置子View
+     * @param w
+     * @param h
+     * @param oldw
+     * @param oldh
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        float xpad = (float) (getPaddingLeft() + getPaddingRight());
+        float ypad = (float) (getPaddingTop() + getPaddingBottom());
 
-        int heightMode = MeasureSpec.getMode(pHeightMeasureSpec);
-        int heightSize = MeasureSpec.getSize(pHeightMeasureSpec);
-
-        switch (heightMode) {
-            case MeasureSpec.AT_MOST:
-            case MeasureSpec.EXACTLY:
-                result = heightSize;
-                break;
-        }
-        return result;
+// Figure out how big we can make the pie.
+//        float diameter = Math.min(ww, hh);
+        mCircleBounds = new RectF(
+                0.0f,
+                0.0f,
+                100.0f,
+                100.0f);
+        circleView.layout((int) mCircleBounds.left,
+                (int) mCircleBounds.top,
+                (int) mCircleBounds.right,
+                (int) mCircleBounds.bottom);
     }
+
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        final int count = getChildCount();
 
-        // These are the far left and right edges in which we are performing layout.
-        int leftPos = getPaddingLeft();
-        int rightPos = getPaddingRight();
-//        int rightPos = r - l - getPaddingRight();
-        for (int i = 0; i < count; i++) {
-            final View child = getChildAt(i);
-            if (child.getVisibility() != GONE) {
-                final LayoutParams lp = (LayoutParams) child.getLayoutParams();
-
-                final int width = child.getMeasuredWidth();
-                final int height = child.getMeasuredHeight();
-
-                // Compute the frame in which we are placing this child.
-                /*if (lp.position == LayoutParams.POSITION_LEFT) {
-                    mTmpContainerRect.left = leftPos + lp.leftMargin;
-                    mTmpContainerRect.right = leftPos + width + lp.rightMargin;
-                    leftPos = mTmpContainerRect.right;
-                } else if (lp.position == LayoutParams.POSITION_RIGHT) {
-                    mTmpContainerRect.right = rightPos - lp.rightMargin;
-                    mTmpContainerRect.left = rightPos - width - lp.leftMargin;
-                    rightPos = mTmpContainerRect.left;
-                } else {
-                    mTmpContainerRect.left = middleLeft + lp.leftMargin;
-                    mTmpContainerRect.right = middleRight - lp.rightMargin;
-                }
-                mTmpContainerRect.top = parentTop + lp.topMargin;
-                mTmpContainerRect.bottom = parentBottom - lp.bottomMargin;*/
-
-                // Use the child's gravity and size to determine its final
-                // frame within its container.
-//                Gravity.apply(lp.gravity, width, height, mTmpContainerRect, mTmpChildRect);
-
-                // Place the child.
-//                child.layout(mTmpChildRect.left, mTmpChildRect.top,
-//                        mTmpChildRect.right, mTmpChildRect.bottom);
-                child.layout(l,width,0,height);
-            }
-        }
     }
 
     private class CircleView extends View {
